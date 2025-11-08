d4ec17fb3adbe0fa0b765d49a4d1fbb3ed22bdf0
giangdc24v7x309 <giangdc24v7x309@dttx.ctu.edu.vn>
2025-11-08 19:57:00 +0700

    Update contact.controller.js

--- contactbook-backend/app/controllers/contact.controller.js
+++ contactbook-backend/app/controllers/contact.controller.js
@@ -58,8 +58,24 @@ exports.findOne = async (req, res, next) => {
     }
 };
 
-exports.update = (req, res) => {
-  res.send({ message: "update handler" });
+// Update a contact by the id in the request
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        return next(new ApiError(400, "Data to update can not be empty"));
+    }
+    
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.update(req.params.id, req.body);
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
+        return res.send({ message: "Contact was updated successfully" });
+    } catch (error) {
+        return next(
+            new ApiError(500, `Error updating contact with id=${req.params.id}`)
+        );
+    }
 };
 
 exports.delete = (req, res) => {
